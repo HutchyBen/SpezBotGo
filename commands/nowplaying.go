6c33f6a92165720974f15dd74c60b46dd001b182
HutchyBean <benhutchy123456@gmail.com>
2022-12-15 18:43:58 +0000

    todo

--- commands/nowplaying.go
+++ commands/nowplaying.go
@@ -1,68 +1,66 @@
-package commands
+// package commands
 
-import (
-	"spezbot/bot"
-	"math"
-	"github.com/bwmarrin/discordgo"
-)
+// import (
+// 	"spezbot/bot"
+// 	"math"
+// 	"github.com/bwmarrin/discordgo"
+// )
 
-func DescProgress()
+// func DescProgress()
 
-func init() {
-	Commands = append(Commands, &bot.Command{
-		ApplicationCommand: discordgo.ApplicationCommand{
-			Type:        discordgo.ChatApplicationCommand,
-			Name:        "nowplaying",
-			Description: "WHAT IS NOW PLAYING IS SHIIIIITTTTTTT (only applies to underworld)",
-		},
+// func init() {
+// 	Commands = append(Commands, &bot.Command{
+// 		ApplicationCommand: discordgo.ApplicationCommand{
+// 			Type:        discordgo.ChatApplicationCommand,
+// 			Name:        "nowplaying",
+// 			Description: "WHAT IS NOW PLAYING IS SHIIIIITTTTTTT (only applies to underworld)",
+// 		},
 
-		Run: func(b *bot.Bot, evt *discordgo.InteractionCreate) *discordgo.InteractionResponseData {
-			vc, ok := b.VoiceInstances[evt.GuildID]
-			if !ok {
-				return &discordgo.InteractionResponseData{
-					Embeds: []*discordgo.MessageEmbed{
-						{
-							Title:       "Error",
-							Description: "not doing anything rn...",
-							Color:       0xff0000,
-						},
-					},
-				}
-			}
+// 		Run: func(b *bot.Bot, evt *discordgo.InteractionCreate) *discordgo.InteractionResponseData {
+// 			vc, ok := b.VoiceInstances[evt.GuildID]
+// 			if !ok {
+// 				return &discordgo.InteractionResponseData{
+// 					Embeds: []*discordgo.MessageEmbed{
+// 						{
+// 							Title:       "Error",
+// 							Description: "not doing anything rn...",
+// 							Color:       0xff0000,
+// 						},
+// 					},
+// 				}
+// 			}
 
-			var prog = float64(vc.PlaybackPosition) / float64(vc.NowPlaying.Track.Length);
-            var emoPos = math.Round(11 * prog);
+// 			var prog = float64(vc.PlaybackPosition) / float64(vc.NowPlaying.Track.Length);
+//             var emoPos = math.Round(11 * prog);
 
-            var seconds = TimeSpan.FromSeconds(Math.Round(connection.CurrentState.PlaybackPosition.TotalSeconds));
-            var str = new StringBuilder("▬▬▬▬▬▬▬▬▬▬▬");
-            str.Remove(emoPos, 1);
-            str.Insert(emoPos, "🔘");
-            str.Append($"`{seconds:hh\\:mm\\:ss}/{song.Length:hh\\:mm\\:ss}`");
-            
-            return str.ToString();
+//             var seconds = TimeSpan.FromSeconds(Math.Round(connection.CurrentState.PlaybackPosition.TotalSeconds));
+//             var str = new StringBuilder("▬▬▬▬▬▬▬▬▬▬▬");
+//             str.Remove(emoPos, 1);
+//             str.Insert(emoPos, "🔘");
+//             str.Append($"`{seconds:hh\\:mm\\:ss}/{song.Length:hh\\:mm\\:ss}`");
 
-			embed := &discordgo.MessageEmbed{
-				Title:       vc.NowPlaying.Track.Title,
-				Description: ,
-				Author: &discordgo.MessageEmbedAuthor{
-					Name:    vc.NowPlaying.Member.User.Username,
-					IconURL: vc.NowPlaying.Member.AvatarURL(1024),
-				},
-				Footer: &discordgo.MessageEmbedFooter{
-					Text: vc.NowPlaying.Track.Author,
-				},
+//             return str.ToString();
 
+// 			embed := &discordgo.MessageEmbed{
+// 				Title:       vc.NowPlaying.Track.Title,
+// 				Description: ,
+// 				Author: &discordgo.MessageEmbedAuthor{
+// 					Name:    vc.NowPlaying.Member.User.Username,
+// 					IconURL: vc.NowPlaying.Member.AvatarURL(1024),
+// 				},
+// 				Footer: &discordgo.MessageEmbedFooter{
+// 					Text: vc.NowPlaying.Track.Author,
+// 				},
 
-
-			return &discordgo.InteractionResponseData{
-				Embeds: []*discordgo.MessageEmbed{
-					{
-						Title:       "Success",
-						Description: "I'm dead wtf....",
-						Color:       0x00ff00,
-					},
-				},
-			}
-		},
-	})
-}
+// 			return &discordgo.InteractionResponseData{
+// 				Embeds: []*discordgo.MessageEmbed{
+// 					{
+// 						Title:       "Success",
+// 						Description: "I'm dead wtf....",
+// 						Color:       0x00ff00,
+// 					},
+// 				},
+// 			}
+// 		},
+// 	})
+// }
