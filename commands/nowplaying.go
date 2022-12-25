68c1f7bc6708bbd0e554b875399cf6190d017fce
HutchyBean <benhutchy123456@gmail.com>
2022-12-25 02:01:21 +0000

    erm testing if for noobs (bellcurve in middle)

--- commands/nowplaying.go
+++ commands/nowplaying.go
@@ -1,66 +1,64 @@
 package commands
 
-// import (
-// 	"spezbot/bot"
-// 	"math"
-// 	"github.com/bwmarrin/discordgo"
-// )
+import (
+	"fmt"
+	"math"
+	"spezbot/bot"
+	"strings"
+	"time"
 
-// func DescProgress()
+	"github.com/bwmarrin/discordgo"
+)
 
-// func init() {
-// 	Commands = append(Commands, &bot.Command{
-// 		ApplicationCommand: discordgo.ApplicationCommand{
-// 			Type:        discordgo.ChatApplicationCommand,
-// 			Name:        "nowplaying",
-// 			Description: "WHAT IS NOW PLAYING IS SHIIIIITTTTTTT (only applies to underworld)",
-// 		},
+func DescProgress(now uint, total uint) string {
+	nowTime := time.Duration(now) * time.Millisecond
+	totalTime := time.Duration(total) * time.Millisecond
+	var prog = float64(nowTime) / float64(totalTime)
+	var emoPos = math.Round(11 * prog)
+	str := "▬▬▬▬▬▬▬▬▬▬▬"
 
-// 		Run: func(b *bot.Bot, evt *discordgo.InteractionCreate) *discordgo.InteractionResponseData {
-// 			vc, ok := b.VoiceInstances[evt.GuildID]
-// 			if !ok {
-// 				return &discordgo.InteractionResponseData{
-// 					Embeds: []*discordgo.MessageEmbed{
-// 						{
-// 							Title:       "Error",
-// 							Description: "not doing anything rn...",
-// 							Color:       0xff0000,
-// 						},
-// 					},
-// 				}
-// 			}
+	tmp := []rune(str)
+	tmp[int(emoPos)] = '🔘'
+	str = string(tmp)
+	return strings.Join([]string{str, fmt.Sprintf("%s / %s", nowTime.String(), totalTime.String())}, " ")
+}
+func init() {
+	Commands = append(Commands, &bot.Command{
+		ApplicationCommand: discordgo.ApplicationCommand{
+			Type:        discordgo.ChatApplicationCommand,
+			Name:        "nowplaying",
+			Description: "WHAT IS NOW PLAYING IS SHIIIIITTTTTTT (only applies to underworld)",
+		},
+		Run: func(b *bot.Bot, evt *discordgo.InteractionCreate) *discordgo.InteractionResponseData {
+			vc, ok := b.VoiceInstances[evt.GuildID]
+			if !ok || vc.NowPlaying == nil || vc.NowPlaying.Track == nil { // not sure if this could happen but just in case
+				return &discordgo.InteractionResponseData{
+					Embeds: []*discordgo.MessageEmbed{
+						{
+							Title:       "Error",
+							Description: "not doing anything rn...",
+							Color:       0xff0000,
+						},
+					},
+				}
+			}
 
-// 			var prog = float64(vc.PlaybackPosition) / float64(vc.NowPlaying.Track.Length);
-//             var emoPos = math.Round(11 * prog);
-
-//             var seconds = TimeSpan.FromSeconds(Math.Round(connection.CurrentState.PlaybackPosition.TotalSeconds));
-//             var str = new StringBuilder("▬▬▬▬▬▬▬▬▬▬▬");
-//             str.Remove(emoPos, 1);
-//             str.Insert(emoPos, "🔘");
-//             str.Append($"`{seconds:hh\\:mm\\:ss}/{song.Length:hh\\:mm\\:ss}`");
-
-//             return str.ToString();
-
-// 			embed := &discordgo.MessageEmbed{
-// 				Title:       vc.NowPlaying.Track.Title,
-// 				Description: ,
-// 				Author: &discordgo.MessageEmbedAuthor{
-// 					Name:    vc.NowPlaying.Member.User.Username,
-// 					IconURL: vc.NowPlaying.Member.AvatarURL(1024),
-// 				},
-// 				Footer: &discordgo.MessageEmbedFooter{
-// 					Text: vc.NowPlaying.Track.Author,
-// 				},
-
-// 			return &discordgo.InteractionResponseData{
-// 				Embeds: []*discordgo.MessageEmbed{
-// 					{
-// 						Title:       "Success",
-// 						Description: "I'm dead wtf....",
-// 						Color:       0x00ff00,
-// 					},
-// 				},
-// 			}
-// 		},
-// 	})
-// }
+			embed := &discordgo.MessageEmbed{
+				Title:       vc.NowPlaying.Track.Title,
+				Description: DescProgress(uint(vc.PlaybackPosition), vc.NowPlaying.Track.Length),
+				Author: &discordgo.MessageEmbedAuthor{
+					Name:    vc.NowPlaying.Member.User.Username,
+					IconURL: vc.NowPlaying.Member.AvatarURL("1024"),
+				},
+				Footer: &discordgo.MessageEmbedFooter{
+					Text: vc.NowPlaying.Track.Author,
+				},
+			}
+			return &discordgo.InteractionResponseData{
+				Embeds: []*discordgo.MessageEmbed{
+					embed,
+				},
+			}
+		},
+	})
+}
